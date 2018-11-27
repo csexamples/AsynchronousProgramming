527b0746ea8d835c84e1fb583f42def432ec45b3
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2018-11-27 20:43:16 +0200

    project files

---
+++ AsynchronousProgramming/Login.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    public class Login
+    {
+        public string LoginMessage { get; set; }
+
+        public string Content { get; set; }
+
+        public async void StartLogin()
+        {
+            try
+            {
+                var loginResult = await LoginAsync();
+                var content = await FetchContent();
+
+                LoginMessage = loginResult;
+                Content = content;
+            }
+            catch (Exception)
+            {
+                LoginMessage = "Internal Error";
+            }
+
+            Console.WriteLine(LoginMessage);
+
+            Console.WriteLine(Content);
+        }
+
+        public async Task<string> FetchContent()
+        {
+            using (var client = new HttpClient())
+            {
+                var httpMessage = await client.GetAsync("http://www.filipekberg.se/rss/");
+
+                var content = await httpMessage.Content.ReadAsStringAsync();
+
+                return content;
+            }
+        }
+
+        public async Task<string> LoginAsync()
+        {
+            try
+            {
+                var loginTask = Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+
+                    return "Login Successful";
+                });
+
+                var logTask = Task.Delay(2000);
+
+                var purchaseTask = Task.Delay(1000);
+
+                await Task.WhenAll(loginTask, logTask, purchaseTask);
+
+                return loginTask.Result;
+            }
+            catch (Exception)
+            {
+                return "Login Failed";
+            }
+
+        }
+    }
+}
---
+++ AsynchronousProgramming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AsynchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var login = new Login();
+
+            login.StartLogin();
+
+            Console.ReadLine();
+        }
+    }
+}
